a45deae9a8e4f7dc45d2b71c5af60858f7c4e965
第十六夜月 <mashanghai88@gmail.com>
2026-08-18 02:48:39 +0800

    Add files via upload

--- luci-app-homeproxy/htdocs/luci-static/resources/view/homeproxy/client.js
+++ luci-app-homeproxy/htdocs/luci-static/resources/view/homeproxy/client.js
@@ -359,7 +359,8 @@ return view.extend({
 		o.depends('main_node', 'urltest');
 
 		o = s.taboption('routing', form.Flag, 'main_urltest_interrupt_exist_connections', _('Interrupt existing connections'));
-		o.default = o.enabled;
+		o.default = o.disabled;
+		o.rmempty = false;
 		o.depends('main_node', 'urltest');
 
 		o = s.taboption('routing', form.ListValue, 'main_core_profile', _('Core config file'));
@@ -425,7 +426,8 @@ return view.extend({
 		o.depends({'main_udp_node': 'urltest', 'main_node': /^((?!core_only).)+$/});
 
 		o = s.taboption('routing', form.Flag, 'main_udp_urltest_interrupt_exist_connections', _('Interrupt existing connections'));
-		o.default = o.enabled;
+		o.default = o.disabled;
+		o.rmempty = false;
 		o.depends({'main_udp_node': 'urltest', 'main_node': /^((?!core_only).)+$/});
 
 		o = s.taboption('routing', form.Value, 'dns_server', _('DNS server'),
